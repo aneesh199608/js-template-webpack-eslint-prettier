574f3d38697495ba48edf44b597e56afcaaadca2
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-03-23 06:39:33 +0530

    feat: add common, dev, prod

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+
+  mode: 'development',
+
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
+
+  devServer: {
+
+    static: './dist',
+    open: true,
+    hot: true,
+
+  },
+
+});
